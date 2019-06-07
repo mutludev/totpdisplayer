b8bfb8ec7da080a62dbff0622fd4e69dbf91d3ca
Baris Mutlu <43097444+5h3ll0@users.noreply.github.com>
2019-06-07 22:59:38 +0300

    Added progress bar

--- reader.py
+++ reader.py
@@ -1,12 +1,17 @@
 import pyotp, datetime, time, os
 from terminaltables import AsciiTable
+
 def print_keys():
     os.system('cls' if os.name == 'nt' else 'clear')
     table_data = [["Name","Key"]]
+    global longest
+    longest = 0
     with open("otp_keys.txt","r") as f:
         for data in f.readlines():
             data = data.replace("\n","")
             name,secret = data.split(":")
+            if len(name) > longest:
+                longest = len(name)
             secret = pyotp.TOTP(secret).now()
             table_data.append([name,secret])
     table = AsciiTable(table_data)
@@ -16,15 +21,17 @@ def print_keys():
 
 def main():
     while True:
-        if(datetime.datetime.now().second == 00 or datetime.datetime.now().second == 30):
+        time_sec = datetime.datetime.now().second
+        if(time_sec == 00 or time_sec == 30):
             print_keys()
             time.sleep(1)
         else:
-            #Print remaining time
-            pass
-
-
-
+            if time_sec <=30:
+                remaining_time = 30-time_sec
+            else:
+                remaining_time = 60-time_sec
+            table_len = longest+13
+            print("#"*(table_len-((remaining_time*table_len)//30)), end="\r")
 
 
 
