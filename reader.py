ca354b06ec94bf1f4188a17b504d2a9a86f3fa1f
Baris Mutlu <43097444+5h3ll0@users.noreply.github.com>
2019-06-07 17:45:48 +0300

    Added Table System and secret to OTP convertion

--- reader.py
+++ reader.py
@@ -1,14 +1,15 @@
-
-
+import pyotp
+from terminaltables import AsciiTable
 def print_keys():
+    table_data = [["Name","Key"]]
     with open("otp_keys.txt","r") as f:
         for data in f.readlines():
             data = data.replace("\n","")
             name,secret = data.split(":")
-            print(name,secret)
-
-
-
+            secret = pyotp.TOTP(secret).now()
+            table_data.append([name,secret])
+    table = AsciiTable(table_data)
+    print(table.table)
 
 def main():
     print_keys()
