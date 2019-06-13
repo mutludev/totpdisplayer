b4960e4d540429ad44f8046b5c70cb468219040d
Baris Mutlu <43097444+5h3ll0@users.noreply.github.com>
2019-06-13 13:23:00 +0300

    styled for pep8 standarts

--- totp_displayer.py
+++ totp_displayer.py
@@ -1,18 +1,22 @@
-import pyotp, datetime, os, time
+import pyotp
+import datetime
+import os
+import time
 from terminaltables import AsciiTable
+
 
 def print_keys():
     os.system('cls' if os.name == 'nt' else 'clear')
-    table_data = [["Name","Key"]]
+    table_data = [["Name", "Key"]]
     longest = 0
-    with open("secrets.txt","r") as f:
+    with open("secrets.txt", "r") as f:
         for data in f.readlines():
-            data = data.replace("\n","")
-            name,secret = data.split(":")
+            data = data.replace("\n", "")
+            name, secret = data.split(":")
             if len(name) > longest:
                 longest = len(name)
             secret = pyotp.TOTP(secret).now()
-            table_data.append([name,secret])
+            table_data.append([name, secret])
     table = AsciiTable(table_data)
     print(table.table)
     return longest
@@ -20,12 +24,13 @@ def print_keys():
 
 def main():
     while True:
+        longest_name = 0
         time_sec = datetime.datetime.now().second
         if(time_sec == 00 or time_sec == 30):
             longest_name = print_keys()
             time.sleep(1)
         else:
-            if time_sec <=30:
+            if time_sec <= 30:
                 remaining_time = 30-time_sec
             else:
                 remaining_time = 60-time_sec
@@ -34,8 +39,8 @@ def main():
             else:
                 remaining_time_str = str(remaining_time)
             table_len = longest_name+11
-            print(remaining_time_str + "#"*(table_len-((remaining_time*table_len)//30)), end="\r")
-
+            loading = "#"*(table_len-((remaining_time*table_len)//30))
+            print(remaining_time_str + loading, end="\r")
 
 
 if __name__ == "__main__":
